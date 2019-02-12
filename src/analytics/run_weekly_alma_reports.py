031ef9cdf77c113fdbd3231ea9b19b3ef6710362
atla5 <aidans@bu.edu>
2019-02-12 09:34:33 -0500

    WEB-70: [refactor] move report content dictionaries inline

--- src/analytics/run_weekly_alma_reports.py
+++ src/analytics/run_weekly_alma_reports.py
@@ -3,44 +3,6 @@ from services.analytics import AlmaAnalytics, PrimoAnalytics
 from os import makedirs
 from os.path import abspath, join, basename
 
-ALMA_REPORTS = {
-    "circulation_stats": {
-        "path": "/shared/Boston University/Reports/jwa/NumberOfLoansPast7days",
-        "output": "circ_stats.tsv"
-    },
-    "openurl_article": {
-        "path": "/shared/Boston University/Reports/jwa/Top ten article title accesses via OpenURL",
-        "output": "open_url_article_stats.tsv"
-    },
-    "openurl_journal": {
-        "path": "/shared/Boston University/Reports/jwa/Top ten title accesses via OpenURL requests",
-        "output": "open_url_title_stats.tsv"
-    },
-    "openurl_request": {
-        "path": "/shared/Boston University/Reports/jwa/Top ten title accesses via OpenURL requests",
-        "output": "open_url_request_stats.tsv"
-    }
-}
-
-PRIMO_API_TEST_REPORTS = {
-    "api_nui": {
-        "path": "/shared/Primo Boston University Libraries/Reports/DataStudio/nuiFacetsForGDS",
-        "output": "nui_actions_for_GDS.tsv"
-    },
-    "api_actions": {
-        "path": "/shared/Primo Boston University Libraries/Reports/API_TestsAPI/Test-ActionsForGDS",
-        "output": "actions_for_GDS.tsv"
-    },
-    "api_facets": {
-        "path": "/shared/Primo Boston University Libraries/Reports/API_Tests/APITest-FacetsForGDS",
-        "output": "facets_for_GDS.tsv"
-    },
-    "api_tabs": {
-        "path": "/shared/Primo Boston University Libraries/Reports/API_Tests/APITest-Tabs",
-        "output": "tabs_for_GDS.tsv"
-    }
-}
-
 # name of the current script, for use in logging
 SCRIPT_NAME = basename(__file__)
 
@@ -68,15 +30,51 @@ def run_reports_from_dictionary(service, reports_dict, project_id="jwasys/bu-lib
 
 
 def run_weekly_circulation_statistics():
+    alma_circulation_reports = {
+        "circulation_stats": {
+            "path": "/shared/Boston University/Reports/jwa/NumberOfLoansPast7days",
+            "output": "circ_stats.tsv"
+        },
+        "openurl_article": {
+            "path": "/shared/Boston University/Reports/jwa/Top ten article title accesses via OpenURL",
+            "output": "open_url_article_stats.tsv"
+        },
+        "openurl_journal": {
+            "path": "/shared/Boston University/Reports/jwa/Top ten title accesses via OpenURL requests",
+            "output": "open_url_title_stats.tsv"
+        },
+        "openurl_request": {
+            "path": "/shared/Boston University/Reports/jwa/Top ten title accesses via OpenURL requests",
+            "output": "open_url_request_stats.tsv"
+        }
+    }
     alma_analytics_svc = AlmaAnalytics(use_production=True)
     output_directory = join(OUTPUT_DIRECTORY, "alma/")
-    run_reports_from_dictionary(alma_analytics_svc, ALMA_REPORTS, output_dir=output_directory)
+    run_reports_from_dictionary(alma_analytics_svc, alma_circulation_reports, output_dir=output_directory)
 
 
 def run_monthly_primo_api_tests():
+    primo_api_test_reports = {
+        "api_nui": {
+            "path": "/shared/Primo Boston University Libraries/Reports/DataStudio/nuiFacetsForGDS",
+            "output": "nui_actions_for_GDS.tsv"
+        },
+        "api_actions": {
+            "path": "/shared/Primo Boston University Libraries/Reports/API_TestsAPI/Test-ActionsForGDS",
+            "output": "actions_for_GDS.tsv"
+        },
+        "api_facets": {
+            "path": "/shared/Primo Boston University Libraries/Reports/API_Tests/APITest-FacetsForGDS",
+            "output": "facets_for_GDS.tsv"
+        },
+        "api_tabs": {
+            "path": "/shared/Primo Boston University Libraries/Reports/API_Tests/APITest-Tabs",
+            "output": "tabs_for_GDS.tsv"
+        }
+    }
     primo_analytics_svc = PrimoAnalytics(use_production=True)
     output_directory = join(OUTPUT_DIRECTORY, "primo/api_tests/")
-    run_reports_from_dictionary(primo_analytics_svc, PRIMO_API_TEST_REPORTS, output_dir=output_directory)
+    run_reports_from_dictionary(primo_analytics_svc, primo_api_test_reports, output_dir=output_directory)
 
 
 if __name__ == "__main__":
